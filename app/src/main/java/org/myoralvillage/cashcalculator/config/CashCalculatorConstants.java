ed39a72b7799a7714228ebb3f84529964601667b
rahulvaish <vaish.rahul4810@gmail.com>
2020-09-01 09:46:30 +0530

    4Share

---
+++ app/src/main/java/org/myoralvillage/cashcalculator/config/CashCalculatorConstants.java
@@ -0,0 +1,28 @@
+package org.myoralvillage.cashcalculator.config;
+
+public final class CashCalculatorConstants {
+
+    public static final int INTRO_VIDEO_WAIT_TIME=1500;
+    public static final int INTRO_VIDEO_ELAPSED_TIME_CALC=1500;
+    public static final int INTRO_VIDEO_ELAPSED_TIME=1000;
+    public static final int INTRO_VIDEO_DURATION_TIME_CALC=1500;
+    public static final int INTRO_VIDEO_DURATION_TIME=1000;
+    public static final int INTRO_VIDEO_FADE_TIME=2000;
+
+    public static final int ADV_VIDEO_WAIT_TIME=1500;
+    public static final int ADV_VIDEO_ELAPSED_TIME_CALC=1500;
+    public static final int ADV_VIDEO_ELAPSED_TIME=1000;
+    public static final int ADV_VIDEO_DURATION_TIME_CALC=1500;
+    public static final int ADV_VIDEO_DURATION_TIME=1000;
+    public static final int ADV_VIDEO_FADE_TIME=2000;
+
+
+    public static final int NUM_VIDEO_WAIT_TIME=1800;
+    public static final int NUM_VIDEO_WAIT_TIME_CURR_STAY_ON_TABLE=3500;
+    public static final int NUM_VIDEO_ELAPSED_TIME_CALC=1800;
+    public static final int NUM_VIDEO_ELAPSED_TIME=1500;
+    public static final int NUM_VIDEO_DURATION_TIME_CALC=1500;
+    public static final int NUM_VIDEO_DURATION_TIME=1000;
+    public static final int NUM_VIDEO_FADE_TIME=2000;
+
+}
